6881f83475733c22bbd6aec867508751fb907a16
shree <shreeyanshukesherwani@gmail.com>
2021-07-19 14:51:52 +0530

    important changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
 import { SigninPageComponent } from './signin-page/signin-page.component';
 import { SetUppageComponent } from './set-uppage/set-uppage.component';
+import { ProfilepageComponent } from './profilepage/profilepage.component';
+
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'loginpage', component:LoginpageComponent},
-  {path:'signingpage', component:SigninPageComponent},
+  {path:'signinpage', component:SigninPageComponent},
   {path:'setuppage', component:SetUppageComponent},
-
+  {path:'profilepage', component:ProfilepageComponent}
 
 ];
 
