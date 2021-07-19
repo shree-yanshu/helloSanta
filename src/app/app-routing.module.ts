9c1dd5ba132cfbfdf6c0faeb3027667a383d05f5
shree <shreeyanshukesherwani@gmail.com>
2021-07-19 14:03:24 +0530

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { SigninPageComponent } from './signin-page/signin-page.component';
+import { SetUppageComponent } from './set-uppage/set-uppage.component';
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:'loginpage', component:LoginpageComponent},
+  {path:'signingpage', component:SigninPageComponent},
+  {path:'setuppage', component:SetUppageComponent},
+
+
 ];
 
 @NgModule({
